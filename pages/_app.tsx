eaef10f3ebcaefbeb1df972596f8b2ebb4beba86
tnagorra <weathermist@gmail.com>
2022-12-29 11:19:21 +0545

    Configure tools
    - Update Readme.md
    - Add .vscode in .gitignore
    - Remove un-necessary dependencies from package.json
    - Update gitignore file
    - Update package.json file
    - Configure eslint
    - Configure postcss
    - Configure stylelint
    - Configure github actions
    - Configure unimported
    - Undo appDir usage
    - Setup base styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/global.css'
-import { AppProps } from 'next/app'
+import 'styles/globals.css';
+import 'styles/variables.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app';
+
+function MyApp(props: AppProps) {
+    const {
+        Component,
+        pageProps,
+    } = props;
+
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    return <Component {...pageProps} />;
 }
+
+export default MyApp;
