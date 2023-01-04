e8a72919506157c7d03c2f37bc30f9d170ed90c3
tnagorra <weathermist@gmail.com>
2023-01-04 10:24:22 +0545

    Use next-export-optimize-images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-};
+const withExportImages = require('next-export-optimize-images');
 
-module.exports = nextConfig
+const nextConfig = withExportImages({
+    reactStrictMode: true,
+    images: {
+        deviceSizes: [640, 960, 1280, 1600, 1920],
+    },
+});
+
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
+
+import flowersData from 'data/flowers.json';
 
 import styles from './styles.module.css';
 
-function Home() {
+interface Flower {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+}
+
+interface Props {
+    flowers: Flower[];
+}
+
+function Home(props: Props) {
+    const {
+        flowers,
+    } = props;
+
     return (
         <div className={styles.page}>
             <Head>
@@ -12,16 +31,47 @@ function Home() {
                 </title>
             </Head>
             <div className={styles.navbar}>
-                Navbar
+                <h2>
+                    Flowers
+                </h2>
             </div>
             <div className={styles.mainContent}>
-                Content
+                {flowers.map((flower) => (
+                    <div
+                        key={flower.id}
+                        className={styles.card}
+                    >
+                        <div className={styles.imageContainer}>
+                            <Image
+                                className={styles.flowerImage}
+                                alt={flower.name}
+                                src={flower.image}
+                                fill
+                            />
+                        </div>
+                        <h2>
+                            {flower.name}
+                        </h2>
+                        <div>
+                            {flower.description}
+                        </div>
+                    </div>
+                ))}
             </div>
             <div className={styles.footer}>
-                Footer
+                Made with 💜 and ☕
             </div>
         </div>
     );
 }
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+    const flowers = flowersData as Flower[];
+    return {
+        props: {
+            flowers,
+        },
+    };
+};
 
 export default Home;
